1e38149c3ae38f8644c42054af4bdd2b4194c378
Muhammet Dinç <gresta.bilisim@gmail.com>
2022-08-29 19:33:16 +0300

    IReser - EfReser Dal Created

---
+++ src/RezervationSystem.DataAccess/Abstract/IReserDal.cs
@@ -0,0 +1,10 @@
+using Core.DataAccess;
+using RezervationSystem.Entity.Concrete;
+
+namespace RezervationSystem.DataAccess.Abstract
+{
+    public interface IReserDal : IRepository<Reser>
+    {
+      
+    }
+}
---
+++ src/RezervationSystem.DataAccess/Concrete/EntityFramework/EfReserRantDal.cs
@@ -0,0 +1,12 @@
+using Core.DataAccess.EntityFramework;
+using RezervationSystem.DataAccess.Abstract;
+using RezervationSystem.DataAccess.Contexts;
+using RezervationSystem.Entity.Concrete;
+
+namespace RezervationSystem.DataAccess.Concrete.EntityFramework
+{
+    public class EfReserRentDal : EfRepositoryBase<ReserRent, RezervationSystemDbContext>, IReserRentDal
+    {
+
+    }
+}
