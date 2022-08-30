142c7d4c2f1288f679b73e060234326511c5b787
emir57 <emir.gurbuz06@hotmail.com>
2022-08-30 19:18:05 +0300

    Add Scopes in Program.cs

--- src/RezervationSystem.WebAPI/Program.cs
+++ src/RezervationSystem.WebAPI/Program.cs
@@ -1,3 +1,8 @@
+using RezervationSystem.Business.Services.Abstract;
+using RezervationSystem.Business.Services.Concrete;
+using RezervationSystem.DataAccess.Abstract;
+using RezervationSystem.DataAccess.Concrete.EntityFramework;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,14 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+#region
+builder.Services.AddScoped<IReserService, ReserManager>();
+//builder.Services.AddScoped<IReserRentService, ReserRentManager>();
+
+builder.Services.AddScoped<IReserDal, EfReserDal>();
+builder.Services.AddScoped<IReserRentDal, EfReserRentDal>();
+#endregion
 
 var app = builder.Build();
 
