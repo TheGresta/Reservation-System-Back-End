ccf29a0b07fe7dad76226c956303f999eb09359d
emir57 <emir.gurbuz06@hotmail.com>
2022-08-30 16:27:03 +0300

    ReserDal

---
+++ src/RezervationSystem.DataAccess/Concrete/EntityFramework/EfReserDal.cs
@@ -0,0 +1,11 @@
+﻿using Core.DataAccess.EntityFramework;
+using RezervationSystem.DataAccess.Abstract;
+using RezervationSystem.DataAccess.Contexts;
+using RezervationSystem.Entity.Concrete;
+
+namespace RezervationSystem.DataAccess.Concrete.EntityFramework
+{
+    public class EfReserDal : EfRepositoryBase<Reser, RezervationSystemDbContext>, IReserDal
+    {
+    }
+}
